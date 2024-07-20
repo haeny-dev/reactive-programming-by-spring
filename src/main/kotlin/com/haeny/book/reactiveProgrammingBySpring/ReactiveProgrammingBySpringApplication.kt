2d67dbacd44184161859edf60df96b5f14ab3261
haeny-dev <wjdeoaks92@gmail.com>
2024-07-20 15:51:49 +0900

    init commit

---
+++ src/main/kotlin/com/haeny/book/reactiveProgrammingBySpring/ReactiveProgrammingBySpringApplication.kt
@@ -0,0 +1,11 @@
+package com.haeny.book.reactiveProgrammingBySpring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactiveProgrammingBySpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReactiveProgrammingBySpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/haeny/book/reactiveProgrammingBySpring/ReactiveProgrammingBySpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.haeny.book.reactiveProgrammingBySpring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveProgrammingBySpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
